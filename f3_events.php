bf9eb775ab47ccbe17b6e95f5e6acb39a36dd99d
Айрат Якупов <jaiq1987@gmail.com>
2016-12-01 21:25:38 +0500

    2 commit

--- f3_events.php
+++ f3_events.php
@@ -1,9 +1,15 @@
 <?php
 /*
+ * Hello everyone by Airat :)
  *
- * Привет всем от Айрата :)
- * This event system need debug and evolution
+ * I was inspired by the Christian Knuth Event System library.
+ * I thank him for that!
+ * And I use the concept of the Christian Knuth library.
+ * I hope that the Christian is not against! :)
+ * https://groups.google.com/d/msg/f3-framework/gKquuIu7Pxo/a81UXkVNFQAJ
+ * Also, I had to remake some of the methods that I picked up from Fat Free Framework Base to integrate Dice DI
  *
+ * This event system need a debug and evolution
  */
 class F3_Events
 {
