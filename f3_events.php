56da6c6bf02eab0262e336c6878783d7d4b19af6
Айрат Якупов <jaiq1987@gmail.com>
2016-12-02 00:40:02 +0500

    Update f3_events.php

--- f3_events.php
+++ f3_events.php
@@ -2,11 +2,10 @@
 /*
  * Hello everyone by Airat :)
  *
- * I was inspired by the Christian Knuth Event System library.
- * I thank him for that!
- * And I use the concept of the Christian Knuth library.
- * I hope that the Christian is not against! :)
+ * I was inspired by the ikkez Event System library.
  * https://groups.google.com/d/msg/f3-framework/gKquuIu7Pxo/a81UXkVNFQAJ
+ * I thank him for that!
+ * And I use the concept of the ikkez library.
  * Also, I had to remake some of the methods that I picked up from Fat Free Framework Base to integrate Dice DI
  *
  * This event system need a debug and evolution
